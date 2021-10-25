d4b12a4ad56de172139c17f24a049cb5c5ad2d2b
SameemaNasrin <sameemanasrin23@gmail.com>
2021-10-25 13:14:50 +0530

    contact form added

---
+++ pages/contact.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        <div class="container contact">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="contact-form">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="name">Name:</label>
+                            <div class="col-sm-10">          
+                            <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Email:</label>
+                            <div class="col-sm-10">
+                            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="message">Message:</label>
+                            <div class="col-sm-10">
+                            <textarea class="form-control" rows="5" id="message" placeholder="Enter message"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">        
+                            <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btnn">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+</script>
