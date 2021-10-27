5e0a2a11a134c06c1a3a3a8f6459b8cdf7ab1127
Pritimoy Sarkar <pritimoysarkar@gmail.com>
2021-10-27 22:11:13 +0530

    ourStory Page added

---
+++ pages/our_story.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Our Story</h1>
+        <div>
+            <h2>History</h2>
+            <p>Our story demo text History</p>
+
+            <h2>Achivements</h2>
+            <p>Our story demo text for Achivements</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
