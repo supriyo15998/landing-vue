1d8e6f356a54535cabc273a814c4b9ddc30bf873
Pritimoy Sarkar <pritimoysarkar@gmail.com>
2021-10-27 22:00:17 +0530

    Users Page added

---
+++ pages/all_user.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>
+            All Users
+        </h1>
+
+        <table>
+            <th>
+                    <td>ID</td>
+                    <td>Name</td>
+            </th>
+            <tr>
+                <td>1</td>
+                <td>Demo Name 1</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Demo Name 2</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Demo Name 3</td>
+            </tr>
+        </table>      
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
